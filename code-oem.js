544a163d2cbafb69313fb43159ea32360ac19fe5
BrunoRuartes <ruartesbruno@gmail.com>
2023-08-20 12:26:19 -0300

    12:26

--- code-oem.js
+++ code-oem.js
@@ -1,11 +1,13 @@
 function toggleFullScreen(iframe) {
     if (!document.fullscreenElement) {
+        iframe.style.display = 'block';
         iframe.requestFullscreen();
         iframe.classList.add('fullscreen');
     } else {
         if (document.exitFullscreen) {
             document.exitFullscreen();
             iframe.classList.remove('fullscreen');
+            iframe.style.display = 'none';
         }
     }
 }
